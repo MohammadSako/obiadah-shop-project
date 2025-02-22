3e84a854eef11c46fa2dbc669ba70a5f8a57176a
obaidahadnan <muhammad.talal.murad@gmail.com>
2025-02-22 18:24:09 +0300

    image fix

--- src/app/[locale]/categories/layout.tsx
+++ src/app/[locale]/categories/layout.tsx
@@ -5,14 +5,11 @@ import Image from "next/image";
 import { CategoryImagesSkeleton } from "@/components/UI/skeletons";
 
 const CategoriesLayout = ({ children }: { children: React.ReactNode }) => {
-  const imageUrl = `${process.env
-    .NEXT_PUBLIC_SUPABASE_URL!}/storage/v1/object/public/shopimages/category_images/store.avif`;
-
   return (
     <>
       <div className="relative flex flex-col w-full h-[20vh]">
         <Image
-          src={imageUrl}
+          src="/store.avif"
           fill
           sizes="100vw"
           alt="Store Image"
